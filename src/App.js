e44f6c9108d7cd1526dcbad8551352dd211374ab
Rachita Singhal <rachita.singhal@soprasteria.com>
2020-07-27 16:16:50 +0530

    Vote up

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.css';
 import './CarouselExample';
+import './VoteUp';
 import CarouselExample from './CarouselExample';
+import VoteUp from './VoteUp';
 function App() {
   return (
+    <div>
     <CarouselExample/>
+    <VoteUp/>
+    </div>
   );
-}
+};
 
 export default App;
